00ca1564edca25dfa4ca62a08c6a0c784ae70422
Mauricio Menegaz <mauricio.menegaz@gmail.com>
2022-08-16 08:01:26 -0300

    model specs

--- app/models/to_do.rb
+++ app/models/to_do.rb
@@ -1,2 +1,7 @@
 class ToDo < ApplicationRecord
+  validates :title, presence: true
+
+  def overdue?
+    due_date.present? && due_date < Date.today
+  end
 end
--- spec/models/to_do_spec.rb
+++ spec/models/to_do_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe ToDo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'overdue?' do
+    it 'past due date' do
+      todo = ToDo.new(title: 'write blog post', due_date: Date.yesterday)
+
+      expect(todo).to be_overdue
+    end
+
+    it 'due today' do
+      todo = ToDo.new(title: 'write blog post', due_date: Date.today)
+
+      expect(todo).to_not be_overdue
+    end
+
+    it 'future due date' do
+      todo = ToDo.new(title: 'write blog post', due_date: Date.tomorrow)
+
+      expect(todo).to_not be_overdue
+    end
+
+    it 'blank due date' do
+      todo = ToDo.new(title: 'write blog post', due_date: nil)
+
+      expect(todo).to_not be_overdue
+    end
+  end
+
 end
